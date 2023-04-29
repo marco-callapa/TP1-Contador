57e23fd0221c7cab53d48133bfeee7374e7884a8
Marco <marcocallapa@fecea.edu.ar>
2023-04-29 02:29:36 -0300

    arregle algo del codigo

--- TP1-Programa/TP1-Programa.ino
+++ TP1-Programa/TP1-Programa.ino
@@ -1,77 +1,90 @@
-//Delcaración de los pines de las teclas
-byte tec1 = 8;
-byte tec2 = 9;
-byte disp1 = 10;
-byte disp2 = 7;
-int display7c[10]= {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x67};
-
-int i = 0; //Variable del contador for
-int uni; //Variable que almacena la unidad 
-int dec; //Variable que almacena la decena 
-long vel = 50; //Velocidad para multeplexacion
+//Vector de valores de display
+int display7c[10]= {0x3f, //Codigo del 0
+                    0x06,
+                    0x5b,
+                    0x4f,
+                    0x66,
+                    0x6d,
+                    0x7d,
+                    0x07,
+                    0x7f,
+                    0x67}; //Codigo del 9
 byte valor; //Almacena el dato de los pulsadores
+int num = 0;  //Variable que almacena el numero a mostrar                   
+int unid; //Variable que almacena la unidad 
+int dec;  //Variable que almacena la decena 
+//Delcaración de Los pines del display
+byte a=2;
+byte b=3;
+byte c=4;
+byte d=5;
+byte e=6;
+byte f=7;
+byte g=8;
+//Delcaración de los pines de control
+byte disp1 = 9;
+byte disp2 = 10;
+//Delcaración de los pines de las teclas
+byte tec1 = 11;
+byte tec2 = 12;
+//Función que coloca en el puerto de salida los bits comenzando
+// desde el pin ini hasta el pin fin
+void puerto(int bits,int ini,int fin){
+  for(int i=ini;i<=fin;i++)
+  {
+    digitalWrite(i,bitRead(bits,i-ini));
+  }
+}
+//Función encargada de la multiplexación
+void mostrar(int num) //Rutina mostrar
+{
+  if (num<=9){
+    digitalWrite(disp1,HIGH); //Energiza la SALIDA DIGITAL (5V)
+      puerto(i,2,8);
+  }
+  else{
+    dec  = num/10;
+    unid = num%10;
+    //Rutina de Multiplexación
+    digitalWrite(disp1,HIGH);  //Enciende el display de decena
+    puerto(display7c[dec],2,8); //Envia la decena al display 1
+    delay(1);               //Retardo de 1 milisegundos
+    digitalWrite(disp1,LOW);  //Apaga el display de decena
+    digitalWrite(disp1,HIGH);  //Enciende el display de unidades
+    puerto(display7c[unid],2,8); ////Envia la unidad al display 2
+    delay(1);               //Retardo de 1 milisegundos
+    digitalWrite(disp2,LOW);   //Apaga el display de unidades
+  }                  
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  //Configura el display como salida
-  DDRD = 1;
+  //Configura los pines del display como salida
+  for(int i=a;i<=disp2;i++){
+    pinMode(i,OUTPUT);
+  }
   // Configura los PINES 30 y 31 como entradas
   pinMode(tec1,INPUT);
   pinMode(tec2,INPUT);
-  // Configura los pines 10 y 7 como salidas
-  pinMode(disp1,OUTPUT);
-  pinMode(disp1,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  PORTD = 0;
   if(digitalRead(tec1)){
-    delay(vel);
-
+    delay(1);
     valor = 1; //Cambia el valor 
     }
+
   if(digitalRead(tec2)){
-    delay(vel);
+    delay(1);
     valor = 0; //Cambia el valor 
   }
+
   if(valor == 1){ 
-    i++;
-    if(i<9){
-      digitalWrite(disp1,HIGH); //Energiza la SALIDA DIGITAL (5V)
-      PORTB = display7c[i];
+    num ++;
+    mostrar(num);
     }
-    if(i> 9){
-      dec = i/10;
-      uni = i%10;
-
-      digitalWrite(disp2,HIGH); //Energiza la SALIDA DIGITAL (5V)
-      PORTB = display7c[dec];
-      delay(vel);
-      digitalWrite(disp2,LOW); //Desenergiza la SALIDA DIGITAL (5V)
-      digitalWrite(disp1,HIGH); //Energiza la SALIDA DIGITAL (5V)
-      PORTB = display7c[uni];
+  else{ 
+    // num --;        
+    mostrar(num);
     }
     
-    }
-  if(valor == 0){ 
-    i--;
-    if(i<9){
-      digitalWrite(disp1,HIGH); //Energiza la SALIDA DIGITAL (5V)
-      PORTB = display7c[i];
-    }
-    if(i> 9){
-      dec = i/10;
-      uni = i%10;
-
-      digitalWrite(disp2,HIGH); //Energiza la SALIDA DIGITAL (5V)
-      PORTB = display7c[dec];
-      delay(vel);
-      digitalWrite(disp2,LOW); //Desenergiza la SALIDA DIGITAL (5V)
-      digitalWrite(disp1,HIGH); //Energiza la SALIDA DIGITAL (5V)
-      PORTB = display7c[uni];
-    }
-    
-    }
-
 }
